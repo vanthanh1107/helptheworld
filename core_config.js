3443d9a863de594308991d951427f7efd2414466
vanthanh1107 <35431584+vanthanh1107@users.noreply.github.com>
2026-07-13 22:38:22 +0700

    Update core_config.js

--- core_config.js
+++ core_config.js
@@ -1,22 +1,14 @@
 window.Game = {
     canvas: null, ctx: null, width: 800, height: 600,
-    
-    // THÊM: Kích thước bản đồ thực tế
-    mapWidth: 3000, 
-    mapHeight: 600,
-    
-    // THÊM: Camera theo dõi người chơi
+    mapWidth: 3000, mapHeight: 600,
     camera: { x: 0, y: 0 },
     
-    players: [], zombies: [], bullets: [], walls: [], crates: [],
+    players: [], zombies: [], bullets: [], walls: [], crates: [], items: [],
+    particles: [], bloodStains: [], // Thêm mảng chứa máu
+    
     keys: { w: false, a: false, s: false, d: false },
     mouse: { x: 0, y: 0, isDown: false, justClicked: false },
-    
-    // THÊM: Tọa độ chuột trên bản đồ thế giới (để nhắm súng chuẩn xác khi map di chuyển)
     worldMouse: { x: 0, y: 0 }, 
     
-    score: 0, 
-    isGameOver: false, 
-    isGameWon: false, // Trạng thái về đích
-    frameCount: 0
+    score: 0, isGameOver: false, isGameWon: false, frameCount: 0
 };
