32dc225349ee0d1c1cfd44bc5b1abf5aedfa0a8a
vanthanh1107 <35431584+vanthanh1107@users.noreply.github.com>
2026-07-13 23:27:13 +0700

    Update network_multiplayer.js

--- network_multiplayer.js
+++ network_multiplayer.js
@@ -1,11 +1,7 @@
 window.NetworkManager = {
     initLocalPlayer: function() {
-        // Tạo một người chơi đặt ở giữa bản đồ
         let localPlayer = new Player(Game.width / 2, Game.height / 2);
         Game.players.push(localPlayer);
-        // Gán là nhân vật chính để camera hoặc phím điều khiển nhận diện
         Game.localPlayer = localPlayer;
     }
-    
-    // TODO sau này: sendPositionToServer(), receiveZombiesFromServer()...
 };
