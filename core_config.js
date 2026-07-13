0558e54ae4cda5d91348d15d0b4827ce43d3cfb5
vanthanh1107 <35431584+vanthanh1107@users.noreply.github.com>
2026-07-13 22:43:36 +0700

    Update core_config.js

--- core_config.js
+++ core_config.js
@@ -1,14 +1,11 @@
 window.Game = {
     canvas: null, ctx: null, width: 800, height: 600,
-    mapWidth: 3000, mapHeight: 600,
-    camera: { x: 0, y: 0 },
+    mapWidth: 3000, mapHeight: 600, camera: { x: 0, y: 0 },
     
-    players: [], zombies: [], bullets: [], walls: [], crates: [], items: [],
-    particles: [], bloodStains: [], 
-    floatingTexts: [], // THÊM MỚI: Mảng chứa số sát thương nảy lên
+    players: [], zombies: [], bullets: [], grenades: [], walls: [], crates: [], items: [],
+    particles: [], bloodStains: [], floatingTexts: [],
     
-    // THÊM MỚI: Nhận diện phím Space
-    keys: { w: false, a: false, s: false, d: false, space: false },
+    keys: { w: false, a: false, s: false, d: false, space: false, r: false, g: false },
     mouse: { x: 0, y: 0, isDown: false, justClicked: false },
     worldMouse: { x: 0, y: 0 }, 
     
