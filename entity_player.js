ef044e0010d28d8a25a4d1a6e1f9da3a6cab2ff9
vanthanh1107 <35431584+vanthanh1107@users.noreply.github.com>
2026-07-13 22:32:37 +0700

    Update entity_player.js

--- entity_player.js
+++ entity_player.js
@@ -2,14 +2,14 @@ class Player {
     constructor(x, y) {
         this.x = x; this.y = y; this.radius = 15;
         this.speed = 3; this.maxHp = 100; this.hp = 100;
-        this.hasArmor = false;
-        this.currentWeapon = 'pistol'; // Vũ khí mặc định
+        this.hasArmor = false; this.currentWeapon = 'pistol';
     }
     update() {
+        // Thay đổi Game.width/height thành Game.mapWidth/mapHeight
         if (Game.keys.w && this.y > this.radius) this.y -= this.speed;
-        if (Game.keys.s && this.y < Game.height - this.radius) this.y += this.speed;
+        if (Game.keys.s && this.y < Game.mapHeight - this.radius) this.y += this.speed;
         if (Game.keys.a && this.x > this.radius) this.x -= this.speed;
-        if (Game.keys.d && this.x < Game.width - this.radius) this.x += this.speed;
+        if (Game.keys.d && this.x < Game.mapWidth - this.radius) this.x += this.speed;
     }
     equipLoot(type) {
         if (type === 'armor' && !this.hasArmor) {
@@ -22,8 +22,10 @@ class Player {
         document.getElementById('hp-display').innerText = "❤️ HP: " + this.hp;
     }
     draw(ctx) {
-        let angle = Physics.getAngle(this.x, this.y, Game.mouse.x, Game.mouse.y);
+        // Nhắm bắn theo tọa độ chuột thực tế trên map
+        let angle = Physics.getAngle(this.x, this.y, Game.worldMouse.x, Game.worldMouse.y);
         ctx.save(); ctx.translate(this.x, this.y); ctx.rotate(angle);
+        
         if (Assets.player.complete) {
             ctx.drawImage(Assets.player, -22.5, -22.5, 45, 45);
             if (this.hasArmor && Assets.armor.complete) ctx.drawImage(Assets.armor, -22.5, -22.5, 45, 45);
