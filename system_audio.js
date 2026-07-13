9e5caad8053477e4ae1532976bfaf9c96f442b52
vanthanh1107 <35431584+vanthanh1107@users.noreply.github.com>
2026-07-13 23:27:35 +0700

    Update system_audio.js

--- system_audio.js
+++ system_audio.js
@@ -4,14 +4,12 @@ window.AudioSys = {
         shoot_machine: "https://raw.githubusercontent.com/kenneynl/Shooter-Down/master/Audio/shoot_machine.ogg",
         shoot_shotgun: "https://raw.githubusercontent.com/kenneynl/Shooter-Down/master/Audio/shoot_shotgun.ogg",
         reload: "https://raw.githubusercontent.com/kenneynl/Shooter-Down/master/Audio/reload.ogg",
-        empty: "https://raw.githubusercontent.com/kenneynl/Shooter-Down/master/Audio/empty.ogg", // Bóp cò khi hết đạn
+        empty: "https://raw.githubusercontent.com/kenneynl/Shooter-Down/master/Audio/empty.ogg", 
         explosion: "https://raw.githubusercontent.com/kenneynl/Shooter-Down/master/Audio/explosion.ogg",
         hit: "https://raw.githubusercontent.com/kenneynl/Shooter-Down/master/Audio/hit.ogg"
     },
     play: function(name, volume = 0.5) {
         if (!this.sounds[name]) return;
-        let audio = new Audio(this.sounds[name]);
-        audio.volume = volume;
-        audio.play().catch(e => {}); // Bỏ qua lỗi tự động play của trình duyệt lúc mới load
+        let audio = new Audio(this.sounds[name]); audio.volume = volume; audio.play().catch(e => {}); 
     }
 };
